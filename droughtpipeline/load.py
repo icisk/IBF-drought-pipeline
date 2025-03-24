faf0a1adefab9c22aa627a2b9b986481d0b7b4f5
marco.renzi <marcorenzi8@gmail.com>
2025-03-24 11:09:24 +0100

    more debug log

--- droughtpipeline/load.py
+++ droughtpipeline/load.py
@@ -147,7 +147,10 @@ class Load:
         """Get administrative boundaries from IBF API"""
         try:
             with open("./data/LSO_adm1.json") as url:
+                logging.info(url)
                 data = json.load(url)
+                logging.info(data)
+                logging.info("....")
                 for ix, record in enumerate(data["features"]):
                     data["features"][ix]["geometry"]["type"] = "MultiPolygon"
                 gdf = gpd.GeoDataFrame.from_features(data)
